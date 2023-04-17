e79e5f4ef07925940f0d7cab6c13a2e117e0eae3
CharlesChen0514 <chenjiale1006@outlook.com>
2023-04-17 15:33:14 +0800

    提供心跳检测功能

--- src/main/java/org/bitkernel/chatserver/Server.java
+++ src/main/java/org/bitkernel/chatserver/Server.java
@@ -1,29 +1,71 @@
 package org.bitkernel.chatserver;
 
-//import lombok.extern.slf4j.Slf4j;
-
+import com.alibaba.fastjson.JSONObject;
+import com.sun.istack.internal.NotNull;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.SocketException;
 
 @Slf4j
 public class Server {
     private static final int UDP_PORT = 9090;
+    private static final int MAX_LEN = 4096;
+    private byte[] inBuff = new byte[MAX_LEN];
+    private DatagramPacket inPacket = new DatagramPacket(inBuff, inBuff.length);
+    private DatagramPacket outPacket;
+    private DatagramSocket socket;
+
+    public Server() {
+        try {
+            socket = new DatagramSocket(UDP_PORT);
+        } catch (SocketException e) {
+            logger.error(e.getMessage());
+        }
+    }
 
     public static void main(String[] args) {
-        try (DatagramSocket socket = new DatagramSocket(UDP_PORT)) {
-            byte[] buffer = new byte[1024];
-            while (true) {
-                DatagramPacket packet = new DatagramPacket(buffer, 0, buffer.length);
-                socket.receive(packet);
-//                System.out.println(packet.getAddress().getHostAddress());
-                String msg = new String(packet.getData(), 0, packet.getLength());
-                logger.info(msg);
-            }
-        } catch (Exception e) {
-            logger.error("{}", e.getMessage());
+        Server s = new Server();
+        while (true) {
+            s.receive();
         }
+    }
 
+    private void receive() {
+        try {
+            socket.receive(inPacket);
+            String msg = new String(inPacket.getData());
+            react(msg);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    /**
+     * code field meaning: <br>
+     * 0 --> heart beat detection
+     * @param msg Json string
+     */
+    private void react(@NotNull String msg) {
+        JSONObject json = JSONObject.parseObject(msg);
+        int code = Integer.parseInt(json.getString("code"));
+        try {
+            switch (code) {
+                case 0:
+                    heartBeating(msg);
+                    break;
+                default:
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void heartBeating(@NotNull String msg) throws IOException {
+        byte[] bytes = msg.getBytes();
+        outPacket = new DatagramPacket(bytes, bytes.length, inPacket.getSocketAddress());
+        socket.send(outPacket);
     }
 }
